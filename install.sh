a6b0d50a85d7530b6e74bf628da4280e047143af
Michael Pabon <mpabon@outlook.com>
2026-01-27 22:27:54 -0500

    Update install script for macos specific installs

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/skhd ~/.config/skhd
-ln -s ~/dotfiles/yabai ~/.config/yabai
-ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/fastfetch ~/.config/fastfetch
+#!/bin/bash
+mkdir -p ~/.config
+
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/fastfetch ~/.config/fastfetch
+
+# macOS only
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ln -sf ~/dotfiles/skhd ~/.config/skhd
+    ln -sf ~/dotfiles/yabai ~/.config/yabai
+fi
+
 echo "Dotfiles installed!"
