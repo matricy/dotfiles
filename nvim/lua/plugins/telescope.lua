b49cac8b1f67292c2f9035b9cc97e762b0cf3060
Michael Pabon <mpabon@outlook.com>
2026-06-01 21:57:28 -0400

    update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,12 +14,13 @@ return {
 					},
 				},
 			})
-
+			require("telescope").load_extension("fzf")
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			vim.keymap.set("n", "<leader>f/", builtin.current_buffer_fuzzy_find, { desc = "Telescope fuzzy find in buffer" })
 		end,
 	},
 }
